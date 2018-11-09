80877158ed298c047a0426dc578b0e82583ec7a4
kayashaolu <kay.ashaolu@gmail.com>
2018-11-09 11:58:58 -0800

    adding infra code

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import firebase from './firebase.js';
+import './style.css';
+
+ReactDOM.render(
+ <div>Loaded Successfully</div>,
+ document.getElementById("root")
+);
